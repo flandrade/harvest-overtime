5ebcbc052cc3eb946192020d8104b1647a014aaa
Fernanda Andrade <flandrade@users.noreply.github.com>
2020-05-16 20:44:42 -0500

    2.2.1

--- src/reporter.ts
+++ src/reporter.ts
@@ -6,7 +6,7 @@ import parseFromReport from "./parser/parser-from-csv";
 import parseToReport from "./parser/parser-to-csv";
 import report from "./report/scraper";
 
-export const CUR_VERSION: string = "2.1.1";
+export const CUR_VERSION: string = "2.2.1";
 export const DEF_INPUT: string = "harvest.csv";
 export const DEF_OUTPUT: string = "report.csv";
 export const DEF_REGULAR_DAY_HOURS: string = "8";
